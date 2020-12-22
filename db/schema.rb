782e0054b62e6063ada5c61ce910d25b693dc0d3
yuu-uym <uyama0816y@gmail.com>
2020-12-22 17:27:21 +0900

    ログイン機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_062720) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.date "join", null: false
+    t.integer "joined", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
