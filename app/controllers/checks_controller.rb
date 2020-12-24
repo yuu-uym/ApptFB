e58ea48bcb71de854fa35802adac0df33309f943
yuu-uym <uyama0816y@gmail.com>
2020-12-24 11:17:28 +0900

    追加ページへの遷移

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,2 +1,6 @@
 class ChecksController < ApplicationController
+  
+  def new
+  end
+
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -1,2 +1,8 @@
 class Check < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :item1
+  end
+
 end
