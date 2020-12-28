579c35fa198676d6d86ff2ae3ffac1228c77e963
yuu-uym <uyama0816y@gmail.com>
2020-12-28 15:55:56 +0900

    チェックモデルの単体テスト

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+describe Check do
+  before do
+    @check = FactoryBot.build(:check)
+  end
 
-RSpec.describe Check, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'チェックリストの登録' do
+    context 'チェックリストの登録がうまくいくとき' do
+      it 'title,item1が存在すれば登録できる' do
+        expect(@check).to be_valid
+      end
+    end
+
+    context 'チェックリストの登録がうまくいかないとき' do
+      it 'titleが空だと登録できない' do
+        @check.title = ''
+        @check.valid?
+        expect(@check.errors.full_messages).to include("タイトルを入力してください")
+      end
+      it 'item1が空だと登録できない' do
+        @check.item1 = ''
+        @check.valid?
+        expect(@check.errors.full_messages).to include("項目１を入力してください")
+      end
+    end
+  end
 end
