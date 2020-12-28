cf98c6fba30e268b5bc91e7d530a49163a946c71
yuu-uym <uyama0816y@gmail.com>
2020-12-28 14:56:37 +0900

    ユーザーモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :joined
-    
   end
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  
   has_many :appointments
   has_many :checks
 end
