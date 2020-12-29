4e274f69ebd741147333e3a07c4767018767ce61
yuu-uym <uyama0816y@gmail.com>
2020-12-29 18:18:38 +0900

    migrate

--- db/migrate/20201225022035_create_appointment_checks.rb
+++
@@ -1,9 +0,0 @@
-class CreateAppointmentChecks < ActiveRecord::Migration[6.0]
-  def change
-    create_table :appointment_checks do |t|
-      t.references :appointment, foreign_key: true
-      t.references :check, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20201225033510_add_check_id_to_appointment.rb
+++
@@ -1,5 +0,0 @@
-class AddCheckIdToAppointment < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :appointments, :check, foreign_key: true
-  end
-end
