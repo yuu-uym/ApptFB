95331c078664b5080592057f4c4e22a173069a7e
yuu-uym <uyama0816y@gmail.com>
2021-01-08 14:13:15 +0900

    レイアウト

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,11 @@
 class AdminsController < ApplicationController
   before_action :admin_user
+  
   def index
     @users = User.all.order("created_at DESC")
+    @checks = Check.all
+    checks = @checks
+    @mychecks = current_user.checks
   end
 
   private
