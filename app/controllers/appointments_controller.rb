2b5f76446e1762dcd1c47a7cefd6a062d2cee685
yuu-uym <uyama0816y@gmail.com>
2021-01-08 16:11:23 +0900

    コメント保存機能の実装

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -60,7 +60,7 @@ class AppointmentsController < ApplicationController
        :point1, :point2, :point3, :point4, :point5, 
        :point6, :point7, :point8, :point9, :point10,
        :point11, :point12, :point13, :point14, :point15, 
-       :point16, :point17, :point18, :point19, :point20)
+       :point16, :point17, :point18, :point19, :point20, :comment)
     .merge(user_id: current_user.id)
   end
 
