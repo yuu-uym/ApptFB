2d2bc997a70264068c55064c020fd566b0b791d0
yuu-uym <uyama0816y@gmail.com>
2021-01-08 15:49:00 +0900

    構造の変更

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,23 @@
 class AdminsController < ApplicationController
   before_action :admin_user
-  
+
   def index
     @users = User.all.order("created_at DESC")
     @checks = Check.all
     checks = @checks
     @mychecks = current_user.checks
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @appointment = Appointment.all.order(appt_date: "DESC")
+    appointment = @appointment
+    @appointment = current_user.appointments
+    @appointment1 = Appointment.where(result_id: [nil, 1], user_id: @user.id ).order(appt_date: "DESC")
+    @appointment2 = Appointment.where(result_id: 2, user_id: @user.id ).order(appt_date: "DESC")
+    @appointment3 = Appointment.where(result_id: 3, user_id: @user.id ).order(appt_date: "DESC")
+    @appointment4 = Appointment.where(result_id: 4, user_id: @user.id ).order(appt_date: "DESC")
+
   end
 
   private
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -28,17 +28,6 @@ class AppointmentsController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-    @appointment = Appointment.all.order(appt_date: "DESC")
-    appointment = @appointment
-    @appointment = current_user.appointments
-    @appointment1 = Appointment.where(result_id: [nil, 1], user_id: @user.id ).order(appt_date: "DESC")
-    @appointment2 = Appointment.where(result_id: 2, user_id: @user.id ).order(appt_date: "DESC")
-    @appointment3 = Appointment.where(result_id: 3, user_id: @user.id ).order(appt_date: "DESC")
-    @appointment4 = Appointment.where(result_id: 4, user_id: @user.id ).order(appt_date: "DESC")
-
-  end
 
   def edit
     @appointment = Appointment.find(params[:id])
