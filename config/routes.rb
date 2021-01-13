a1601737d1223be8ef71cfe395521d5c98ce0ee6
yuu-uym <uyama0816y@gmail.com>
2021-01-13 16:30:51 +0900

    エラー解決のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'appointments#index'
-  
+  post 'appointments/:id/evaluations/:id', to: 'evaluations#create'
+  root to: 'appointments#index' 
   resources :checks
   resources :admins, only: [:index, :show]
   resources :appointments do
    resources :evaluations, only: [:index, :create, :edit, :update]
-   post 'appointments/:id/evaluations/:id', to: 'evaluations#create'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
